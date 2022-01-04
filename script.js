b8cb44b0d97cd107c6b6798922bf62b8f8496fd2
Daiana Ortiz <daianayortiz@live.com>
2022-01-04 19:50:51 -0300

    add api request

--- script.js
+++ script.js
@@ -55,3 +55,33 @@ document.
     updateSlidePosition();
   }
 
+
+// MARVEL API 
+
+const marvel = {
+  render: () => {
+    const urlAPI = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=thor&ts=1&apikey=f1c50c9a6917b8f09f5e68d9adebf69d&hash=721cf87c33af1246d1b0e9b8307fb546";
+    const container = document.querySelector('#marvel-row');
+    let contentHTML = '';
+
+    fetch(urlAPI)
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json,)
+        for (const hero of json.data.results) {
+          let urlHero = hero.urls[0].url;
+          contentHTML += `
+            <div class="character">
+              <div class="imagen">
+                <a href="${urlHero}" target="_blank">
+                  <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="img-thumbnail">
+                </a>
+                <h3>${hero.name}</h3>
+                </div>
+            </div>`;
+        }
+        container.innerHTML = contentHTML;
+      })
+  }
+};
+marvel.render();
